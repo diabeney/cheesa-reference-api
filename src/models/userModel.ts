2d41257f5caafe34a06305a84bc30b89b9d7a407
Evans <ellaboevans@gmail.com>
2024-01-01 20:50:27 +0000

    feat(app): Added reset password methods

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,14 +1,30 @@
-import { Schema, model } from "mongoose";
-import { IUser } from "../types/types";
+import { Schema, model } from 'mongoose'
+import { IUser } from '../types/types'
+import crypto from 'crypto'
 
 const UsersSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ["lecturer", "graduate"] },
-});
+  role: { type: String, enum: ['lecturer', 'graduate'] },
+  resetPasswordToken: { type: String },
+  resetPasswordExpires: { type: Date }
+})
 
-const Users = model<IUser>("Users", UsersSchema);
+UsersSchema.methods.getResetPasswordToken = function () {
+  const resetToken = crypto.randomBytes(20).toString('hex')
 
-export default Users;
+  this.resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex')
+
+  this.resetPasswordExpires = Date.now() + 10 * (60 * 1000) //Ten Minutes
+
+  return resetToken
+}
+
+const Users = model<IUser | any>('Users', UsersSchema)
+
+export default Users
