5d2a28a2a7a1494fb51ebdb35f36efb442f0ebfe
Evans <ellaboevans@gmail.com>
2024-01-03 12:33:20 +0000

    feat(server): Added VerifyToken middle to the payment route
    To ensure the user has a valid token before proceeding to make payment on their request

--- src/routes/versionOne/index.ts
+++ src/routes/versionOne/index.ts
@@ -12,7 +12,7 @@ router.get('/', (_, res) => res.send('Hello'))
 router.use('/auth', AuthRoutes)
 router.use('/users', verifyToken, UserRoutes)
 router.use('/reference', verifyToken, ReferenceRoutes)
-router.use('/payment', PaymentRoutes)
+router.use('/payment', verifyToken, PaymentRoutes)
 
 // the /posts route is for testing purposes
 router.get('/posts', verifyToken, (_, res) => {
