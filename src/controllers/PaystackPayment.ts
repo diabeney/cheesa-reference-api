414fe09ea12f581556601cec9234c90c81e45771
Evans <ellaboevans@gmail.com>
2024-01-03 12:31:56 +0000

    fix(server): Get Logged in user email to proceed payment
    This logic extracts the logged in user's email if successfully logged in and have a valid accessToken to enable them to make payment without having to input their emails again

--- src/controllers/PaystackPayment.ts
+++ src/controllers/PaystackPayment.ts
@@ -1,51 +1,69 @@
 require('dotenv').config()
 import https from 'https'
 import { Request, Response } from 'express'
-
-const BY_HUNDRED = 100
-const STATIC_AMOUNT = 30
-
-const TOTAL_AMOUNT = STATIC_AMOUNT * BY_HUNDRED
+import { getUserByEmail } from '../db/user'
+import { AuthRequest } from '../types/types'
+import { ErrorMsg } from '../utils'
+import { TOTAL_AMOUNT } from '../constants/constants'
 
 const payStack = {
   // Handle Payment Controller (Accept Payment)
-  handlePayment: async (req: Request, res: Response) => {
+  handlePayment: async (req: AuthRequest, res: Response) => {
+    const user = req.userPayload
     try {
-      const { email, quantity } = req.body
-      // params from the body
-      const params = JSON.stringify({
-        email,
-        amount: quantity ? quantity * TOTAL_AMOUNT : TOTAL_AMOUNT
-      })
-      // options
-      const options = {
-        hostname: process.env.PAYSTACK_HOST,
-        port: process.env.PAYSTACK_PORT,
-        path: '/transaction/initialize',
-        method: 'POST',
-        headers: {
-          Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
-          'Content-Type': 'application/json'
+      /*  Look for user in the database
+          and get the email to use for payment
+          after they have been authenticated and
+          logged in successfully
+      */
+      if (user) {
+        const { email } = user
+        const foundUser = await getUserByEmail(email)
+
+        if (!foundUser) return res.status(404).json(ErrorMsg(400))
+
+        //Get logged in user email if found
+        const { email: logged_in_user_email } = foundUser
+        console.log(logged_in_user_email)
+        //Get quantity from req.body
+        const { quantity } = req.body
+
+        // params from the body
+
+        const params = JSON.stringify({
+          email: logged_in_user_email,
+          amount: quantity ? quantity * TOTAL_AMOUNT : TOTAL_AMOUNT
+        })
+        // options
+        const options = {
+          hostname: process.env.PAYSTACK_HOST,
+          port: process.env.PAYSTACK_PORT,
+          path: '/transaction/initialize',
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${process.env.PAYSTACK_SECRET_KEY}`,
+            'Content-Type': 'application/json'
+          }
         }
+        // client request
+        const client_request = https
+          .request(options, (api_response) => {
+            let data = ''
+            api_response.on('data', (chunk) => {
+              data += chunk
+            })
+            api_response.on('end', () => {
+              console.log(JSON.parse(data))
+              return res.status(200).json(data)
+            })
+          })
+          .on('error', (error) => {
+            console.error(error)
+            return res.status(400).json(error.message)
+          })
+        client_request.write(params)
+        client_request.end()
       }
-      // client request
-      const client_request = https
-        .request(options, (api_response) => {
-          let data = ''
-          api_response.on('data', (chunk) => {
-            data += chunk
-          })
-          api_response.on('end', () => {
-            console.log(JSON.parse(data))
-            return res.status(200).json(data)
-          })
-        })
-        .on('error', (error) => {
-          console.error(error)
-          return res.status(400).json(error.message)
-        })
-      client_request.write(params)
-      client_request.end()
     } catch (error) {
       // Handle any errors that occur during the request
       console.error(error)
