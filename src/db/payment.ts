fffce55ed7486c3038813e08071791f23e1b9b54
Evans <ellaboevans@gmail.com>
2024-01-05 00:12:48 +0000

    feat(server): Add logic to get all payments and total amounts

--- src/db/payment.ts
+++ src/db/payment.ts
@@ -11,11 +11,13 @@ const PaystackPayments = {
     if (!userId) return res.status(400).json(ErrorMsg(400))
 
     try {
-      const payments = await Payments.find({ userId }).populate({
-        path: 'userId',
-        select: 'firstName, lastName, email',
-        model: Users
-      })
+      const payments = await Payments.find({ userId })
+        .populate({
+          path: 'userId',
+          select: 'firstName, lastName, email',
+          model: Users
+        })
+        .sort({ createdAt: -1 })
 
       /*  Checking the length of the payments tank
           to return a reasonable reponse
@@ -29,7 +31,30 @@ const PaystackPayments = {
       console.error(error)
       return res.status(500).json(ErrorMsg(500))
     }
+  },
+  getAllPayments: async (req: Request, res: Response) => {
+    try {
+      const allPayments = await Payments.find().sort({ createdAt: -1 })
+
+      /*  Checking the length of the all payments tank
+          to return a reasonable reponse
+      */
+      if (allPayments.length <= 0) {
+        return res.status(404).json(ErrorMsg(404, 'There are no payments yet!'))
+      }
+
+      if (allPayments) {
+        // Get all the amounts from the payments
+        const amounts = allPayments.map((payment) => payment.amount)
+        // Sum up all the amounts
+        const totalAmount = amounts.reduce((a, b) => a + b, 0)
+        return res.status(200).json({ allPayments, totalAmount })
+      }
+    } catch (error) {
+      console.error(error)
+      return res.status(500).json(ErrorMsg(500))
+    }
   }
 }
 
-export const { getPaymentsByUserId } = PaystackPayments
+export const { getPaymentsByUserId, getAllPayments } = PaystackPayments
