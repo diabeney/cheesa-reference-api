7fab6e1e3591851c5af51d43c56576171a4a2ec1
Evans <ellaboevans@gmail.com>
2024-01-06 00:53:28 +0000

    feat(server): Added Verify Email route

--- src/routes/versionOne/auth.ts
+++ src/routes/versionOne/auth.ts
@@ -4,9 +4,9 @@ import {
   handleRefreshToken,
   handleSignUp
 } from '../../controllers/auth'
-
 import { resetPassword } from '../../controllers/resetPassword'
 import { forgotPassword } from '../../controllers/forgotPassword'
+import { verifyEmailToken } from '../../controllers/verifyEmailToken'
 
 const router = express.Router()
 
@@ -15,5 +15,6 @@ router.post('/login', handleLogin)
 router.post('/refresh', handleRefreshToken)
 router.post('/forgot-password', forgotPassword)
 router.post('/reset-password', resetPassword)
+router.get('/verify/:token', verifyEmailToken)
 
 export { router as AuthRoutes }
