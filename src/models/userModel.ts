f1e75f89f5b09ebd6d2fa9f11a972d8d0d777566
Evans <ellaboevans@gmail.com>
2024-01-07 20:09:22 +0000

    fix(server): Added union any type

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -3,31 +3,31 @@ import { IUser } from "../types/types";
 import crypto from "crypto";
 
 const UsersSchema = new Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ["lecturer", "graduate"] },
-  referenceNumber: { type: String, required: true },
-  indexNumber: { type: String, required: false },
-  resetPasswordToken: { type: String },
-  resetPasswordExpires: { type: Date },
-  isVerified: { type: Boolean, default: false },
+	firstName: { type: String, required: true },
+	lastName: { type: String, required: true },
+	email: { type: String, required: true },
+	password: { type: String, required: true },
+	role: { type: String, enum: ["lecturer", "graduate"] },
+	referenceNumber: { type: String, required: true },
+	indexNumber: { type: String, required: false },
+	resetPasswordToken: { type: String },
+	resetPasswordExpires: { type: Date },
+	isVerified: { type: Boolean, default: false },
 });
 
 UsersSchema.methods.getResetPasswordToken = function () {
-  const resetToken = crypto.randomBytes(20).toString("hex");
+	const resetToken = crypto.randomBytes(20).toString("hex");
 
-  this.resetPasswordToken = crypto
-    .createHash("sha256")
-    .update(resetToken)
-    .digest("hex");
+	this.resetPasswordToken = crypto
+		.createHash("sha256")
+		.update(resetToken)
+		.digest("hex");
 
-  this.resetPasswordExpires = Date.now() + 10 * (60 * 1000); //Ten Minutes
+	this.resetPasswordExpires = Date.now() + 10 * (60 * 1000); //Ten Minutes
 
-  return resetToken;
+	return resetToken;
 };
 
-const Users = model<IUser>("Users", UsersSchema);
+const Users = model<IUser | any>("Users", UsersSchema);
 
 export default Users;
