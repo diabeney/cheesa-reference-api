2dc4f25a356ac39443a7dcd7488d25d3548aa0e2
Diabene <addodiabene69@gmail.com>
2024-01-08 20:23:28 +0000

    chore: add firstName & lastName to lecturers ref response

--- src/db/reference.ts
+++ src/db/reference.ts
@@ -4,98 +4,98 @@ import Users from "../models/userModel";
 import { IReferenceRequest } from "../types/types";
 
 const RequestReference = async (payload: IReferenceRequest) =>
-	await new Reference(payload).save();
+  await new Reference(payload).save();
 
 const getReferenceById = async (id: string) => {
-	const result = await Reference.findOne({ _id: id })
-		.populate({
-			path: "graduateId lecturerId",
-			select: "firstName lastName email indexNumber referenceNumber",
-			model: Users,
-		})
-		.then((reference) => {
-			if (reference) {
-				return {
-					id: reference._id,
-					graduateInfo: reference.graduateId,
-					lecturer: reference.lecturerId,
-					programme: reference.programme,
-					graduationYear: reference.graduationYear,
-					destination: reference.destination,
-					expectedDate: reference.expectedDate,
-					transactionStatus: reference.transactionStatus,
-					createdAt: reference.createdAt,
-					accepted: reference.accepted,
-					status: reference.status,
-				};
-			}
-		});
-	return result;
+  const result = await Reference.findOne({ _id: id })
+    .populate({
+      path: "graduateId lecturerId",
+      select: "firstName lastName email indexNumber referenceNumber",
+      model: Users,
+    })
+    .then((reference) => {
+      if (reference) {
+        return {
+          id: reference._id,
+          graduateInfo: reference.graduateId,
+          lecturer: reference.lecturerId,
+          programme: reference.programme,
+          graduationYear: reference.graduationYear,
+          destination: reference.destination,
+          expectedDate: reference.expectedDate,
+          transactionStatus: reference.transactionStatus,
+          createdAt: reference.createdAt,
+          accepted: reference.accepted,
+          status: reference.status,
+        };
+      }
+    });
+  return result;
 };
 
 const getUsersReferenceByRole = async (
-	id: string,
-	role: "lecturer" | "graduate",
+  id: string,
+  role: "lecturer" | "graduate"
 ) => {
-	if (role === "graduate") {
-		const result = await Reference.find({ graduateId: id })
-			.populate({
-				path: "graduateId lecturerId",
-				select: "indexNumber referenceNumber",
-				model: Users,
-			})
-			.sort({ createdAt: -1 })
-			.then((reference) => {
-				return reference.map((reference) => ({
-					id: reference._id,
-					graduateInfo: reference.graduateId,
-					lecturer: reference.lecturerId,
-					programme: reference.programme,
-					graduationYear: reference.graduationYear,
-					destination: reference.destination,
-					expectedDate: reference.expectedDate,
-					transactionStatus: reference.transactionStatus,
-					createdAt: reference.createdAt,
-					accepted: reference.accepted,
-					status: reference.status,
-				}));
-			});
-		return result;
-	}
+  if (role === "graduate") {
+    const result = await Reference.find({ graduateId: id })
+      .populate({
+        path: "graduateId lecturerId",
+        select: "indexNumber referenceNumber",
+        model: Users,
+      })
+      .sort({ createdAt: -1 })
+      .then((reference) => {
+        return reference.map((reference) => ({
+          id: reference._id,
+          graduateInfo: reference.graduateId,
+          lecturer: reference.lecturerId,
+          programme: reference.programme,
+          graduationYear: reference.graduationYear,
+          destination: reference.destination,
+          expectedDate: reference.expectedDate,
+          transactionStatus: reference.transactionStatus,
+          createdAt: reference.createdAt,
+          accepted: reference.accepted,
+          status: reference.status,
+        }));
+      });
+    return result;
+  }
 
-	const result = await Reference.find({ lecturerId: id })
-		.populate({
-			path: "lecturerId graduateId",
-			select: "referenceNumber indexNumber",
-			model: Users,
-		})
-		.sort({ createdAt: -1 })
-		.then((reference) => {
-			return reference.map((reference) => ({
-				id: reference._id,
-				graduateInfo: reference.graduateId,
-				lecturer: reference.lecturerId,
-				programme: reference.programme,
-				graduationYear: reference.graduationYear,
-				destination: reference.destination,
-				expectedDate: reference.expectedDate,
-				transactionStatus: reference.transactionStatus,
-				createdAt: reference.createdAt,
-				accepted: reference.accepted,
-				status: reference.status,
-			}));
-		});
-	return result;
+  const result = await Reference.find({ lecturerId: id })
+    .populate({
+      path: "lecturerId graduateId",
+      select: "referenceNumber indexNumber firstName lastName",
+      model: Users,
+    })
+    .sort({ createdAt: -1 })
+    .then((reference) => {
+      return reference.map((reference) => ({
+        id: reference._id,
+        graduateInfo: reference.graduateId,
+        lecturer: reference.lecturerId,
+        programme: reference.programme,
+        graduationYear: reference.graduationYear,
+        destination: reference.destination,
+        expectedDate: reference.expectedDate,
+        transactionStatus: reference.transactionStatus,
+        createdAt: reference.createdAt,
+        accepted: reference.accepted,
+        status: reference.status,
+      }));
+    });
+  return result;
 };
 
 const updateReferenceById = async (
-	id: Types.ObjectId,
-	payload: Pick<IReferenceRequest, "accepted">,
+  id: Types.ObjectId,
+  payload: Pick<IReferenceRequest, "accepted">
 ) => await Reference.findOneAndUpdate({ _id: id }, payload, { new: true });
 
 export {
-	RequestReference,
-	getUsersReferenceByRole,
-	updateReferenceById,
-	getReferenceById,
+  RequestReference,
+  getUsersReferenceByRole,
+  updateReferenceById,
+  getReferenceById,
 };
