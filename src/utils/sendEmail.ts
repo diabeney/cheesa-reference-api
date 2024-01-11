a5437d6d99c62ff9784b27635744e1f5f088f4b6
Elabo <ellaboevans@gmail.com>
2024-01-11 14:19:51 +0000

    refactor(server): Containerize Email Fire Functions

---
+++ src/utils/sendEmail.ts
@@ -0,0 +1,33 @@
+import { Options } from '../types/types'
+import { nodemailerTransporter } from './nodemailer'
+
+const sendResetPasswordEmail = async (options: Options) => {
+  const transporter = nodemailerTransporter()
+
+  const message = {
+    from: `REFHUB <${process.env.SMTP_USERNAME}>`,
+    to: options.to,
+    subject: options.subject,
+    html: options.message
+  }
+
+  await transporter.sendMail(message)
+  
+}
+
+const sendVerificationEmail = async (options: Options) => {
+  const transporter = nodemailerTransporter()
+
+  const message = {
+    from: `REFHUB <${process.env.SMTP_USERNAME}>`,
+    to: options.to,
+    subject: options.subject,
+    html: options.message
+  }
+
+  await transporter.sendMail(message)
+}
+
+
+
+export { sendResetPasswordEmail, sendVerificationEmail }
