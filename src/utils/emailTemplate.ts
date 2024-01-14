8d03a6731f4076d58fbf7976bb218faf525aba8c
Elabo <ellaboevans@gmail.com>
2024-01-14 02:05:29 +0000

    feat(server): Added various mail templates

--- src/utils/emailTemplate.ts
+++ src/utils/emailTemplate.ts
@@ -327,7 +327,7 @@ const isAcceptedMessage = () => {
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Payment verification</title>
+    <title>Acceptance Notice</title>
   </head>
 
   <body style="font-family: Helvetica, Arial, sans-serif; margin: 0px; padding: 0px; background-color: #ffffff;">
@@ -391,4 +391,144 @@ const isAcceptedMessage = () => {
    return html
 }
 
-export { forgotPasswordMessage, EmailVerificationMessage, PaymentVerificationMessage, LecturerPaymentConfirmationMessage, isAcceptedMessage };
+const isRejectedMessage = () => {
+  const html = `
+    <html xmlns="http://www.w3.org/1999/xhtml">
+
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceptance Notice</title>
+  </head>
+
+  <body style="font-family: Helvetica, Arial, sans-serif; margin: 0px; padding: 0px; background-color: #ffffff;">
+    <table role="presentation" style="width: 100%; border-collapse: collapse; border: 0px; border-spacing: 0px; font-family: Arial, Helvetica, sans-serif; background-color: rgb(239, 239, 239);">
+      <tbody>
+        <tr>
+          <td align="center" style="padding: 1rem; vertical-align: top; width: 100%;">
+            <table role="presentation" style="max-width:700px; border-collapse: collapse; border: 0px; border-spacing: 0px; text-align: left;">
+              <tbody>
+                <tr>
+                  <td style="padding: 40px 0px 0px;">
+                    <div style="text-align: center; margin-bottom: 0.5rem;">
+                      <div style="display: flex; align-items: center; justify-center; width: 100%; padding: 10px; background: white margin-bottom: ">
+                        <img src="https://ik.imagekit.io/kkldhhslb/RefHub_logo_final.png?updatedAt=1704396221616" alt="Company-Logo" style="width:120px">
+                      </div>
+                    </div>
+                    <div style=" padding: 20px; background-color: rgb(255, 255, 255); border-radius: 0.5rem;">
+                      <div style="color: rgb(46, 46, 46); text-align: left;">
+                        <h1 style="margin: 1rem 0">Request Decline Notice</h1>
+                        <h3>Hello,</h3>
+                        <p style="padding-bottom: 16px">Sorry!😢 The request you made for a recommendation letter to be sent to your selected institution has been <strong>DECLINED</strong> by the assigned lecturer.</p>
+                         <p style="padding-bottom: 16px">If you have any questions or concerns, please don't hesitate to contact us.</p>
+                        </p>
+                        <p style="padding-bottom: 16px">
+                          <span>Thanks,</span>
+                          <br>
+                          <span style="font-weight: bold;">RefHub Support Team</span>
+                        </p>
+                      </div>
+                    </div>
+                    <div style="color: rgb(153, 153, 153); text-align: center; padding: 1rem;">
+                      <p>
+                       <span>&copy; ${new Date().getFullYear()} RefHub. All rights reserved.</span>
+                       <br>
+                        <span>A dedicated Platform to assist Graduates.</span>
+                        <br>
+                        <span> <a href="https://knust.edu.gh" target="_blank" rel="noopener noreferrer" style="text-decoration: underline; color: inherit">KNUST</a>, Kumasi-Ghana</span>
+                      </p>
+                       <small style="padding-bottom: 16px;">
+                         Proudly built by the engineers at 
+                        <span style="font-weight: 700;">
+                         <a href="#" target="_blank" rel="noopener noreferrer"  style="text-decoration: none; color: inherit">
+                          InteliTech Inc.
+                         </a>
+                        </span>
+                      </small>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </body>
+
+</html>
+   `
+   return html
+}
+
+const isSubmittedMessage = () => {
+  const html = `
+    <html xmlns="http://www.w3.org/1999/xhtml">
+
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceptance Notice</title>
+  </head>
+
+  <body style="font-family: Helvetica, Arial, sans-serif; margin: 0px; padding: 0px; background-color: #ffffff;">
+    <table role="presentation" style="width: 100%; border-collapse: collapse; border: 0px; border-spacing: 0px; font-family: Arial, Helvetica, sans-serif; background-color: rgb(239, 239, 239);">
+      <tbody>
+        <tr>
+          <td align="center" style="padding: 1rem; vertical-align: top; width: 100%;">
+            <table role="presentation" style="max-width:700px; border-collapse: collapse; border: 0px; border-spacing: 0px; text-align: left;">
+              <tbody>
+                <tr>
+                  <td style="padding: 40px 0px 0px;">
+                    <div style="text-align: center; margin-bottom: 0.5rem;">
+                      <div style="display: flex; align-items: center; justify-center; width: 100%; padding: 10px; background: white margin-bottom: ">
+                        <img src="https://ik.imagekit.io/kkldhhslb/RefHub_logo_final.png?updatedAt=1704396221616" alt="Company-Logo" style="width:120px">
+                      </div>
+                    </div>
+                    <div style=" padding: 20px; background-color: rgb(255, 255, 255); border-radius: 0.5rem;">
+                      <div style="color: rgb(46, 46, 46); text-align: left;">
+                        <h1 style="margin: 1rem 0">Request Submission Notice</h1>
+                        <h3>Hello,</h3>
+                        <p style="padding-bottom: 16px"><strong>Woohoo!🎊 Congrats on your successful recommendation submission!</strong></p>
+                        <p style="padding-bottom: 16px">Your request just got the green light! The assigned lecturer has sent off your recommendation to your chosen institution, ready to champion your amazing potential.</p>
+                         <p style="padding-bottom: 16px"> We wish you the best of luck in your future endeavors!</p>
+                        </p>
+                        <p style="padding-bottom: 16px">
+                          <span>Best Regards,</span>
+                          <br>
+                          <span style="font-weight: bold;">RefHub Support Team</span>
+                        </p>
+                      </div>
+                    </div>
+                    <div style="color: rgb(153, 153, 153); text-align: center; padding: 1rem;">
+                      <p>
+                       <span>&copy; ${new Date().getFullYear()} RefHub. All rights reserved.</span>
+                       <br>
+                        <span>A dedicated Platform to assist Graduates.</span>
+                        <br>
+                        <span> <a href="https://knust.edu.gh" target="_blank" rel="noopener noreferrer" style="text-decoration: underline; color: inherit">KNUST</a>, Kumasi-Ghana</span>
+                      </p>
+                       <small style="padding-bottom: 16px;">
+                         Proudly built by the engineers at 
+                        <span style="font-weight: 700;">
+                         <a href="#" target="_blank" rel="noopener noreferrer"  style="text-decoration: none; color: inherit">
+                          InteliTech Inc.
+                         </a>
+                        </span>
+                      </small>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </body>
+
+</html>
+  `
+    return html
+}
+export { forgotPasswordMessage, EmailVerificationMessage, PaymentVerificationMessage, LecturerPaymentConfirmationMessage, isAcceptedMessage, isRejectedMessage, isSubmittedMessage };
