6cbdc8b724d3702ed3d9793a888926f093f9ac00
Diabene <addodiabene69@gmail.com>
2024-02-18 20:01:00 +0000

    chore: add cwa

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -9,143 +9,145 @@ import { adminUpdateShape } from "../constants/constants";
 import { ZodError } from "zod";
 
 async function handleGetLoggedInUser(req: AuthRequest, res: Response) {
-	const user = req.userPayload;
+  const user = req.userPayload;
 
-	try {
-		if (user) {
-			const { email } = user;
-			const foundUser = await getUserByEmail(email);
+  try {
+    if (user) {
+      const { email } = user;
+      const foundUser = await getUserByEmail(email);
 
-			if (!foundUser) return res.status(404).json(ErrorMsg(400));
-			const {
-				_id,
-				firstName,
-				lastName,
-				role,
-				email: userEmail,
-				entryYear,
-				graduationYear,
-				indexNumber,
-				referenceNumber,
-				programme,
-				projects,
-				placeOfWork,
-				nss,
-				telephone,
-				isVerified,
-			} = foundUser;
+      if (!foundUser) return res.status(404).json(ErrorMsg(400));
+      const {
+        _id,
+        firstName,
+        lastName,
+        role,
+        email: userEmail,
+        entryYear,
+        graduationYear,
+        indexNumber,
+        referenceNumber,
+        programme,
+        projects,
+        placeOfWork,
+        nss,
+        telephone,
+        isVerified,
+        cwa,
+      } = foundUser;
 
-			return res.status(200).json({
-				id: _id,
-				firstName,
-				lastName,
-				role,
-				email: userEmail,
-				entryYear,
-				graduationYear,
-				indexNumber,
-				referenceNumber,
-				programme,
-				projects,
-				placeOfWork,
-				nss,
-				telephone,
-				isVerified,
-			});
-		}
-		return res.status(401).json(ErrorMsg(401));
-	} catch (error) {
-		console.log(error);
-		res.status(500).json(ErrorMsg(500));
-	}
+      return res.status(200).json({
+        id: _id,
+        firstName,
+        lastName,
+        role,
+        email: userEmail,
+        entryYear,
+        graduationYear,
+        indexNumber,
+        referenceNumber,
+        programme,
+        projects,
+        placeOfWork,
+        nss,
+        telephone,
+        isVerified,
+        cwa,
+      });
+    }
+    return res.status(401).json(ErrorMsg(401));
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(ErrorMsg(500));
+  }
 }
 
 async function handleGetUsers(req: AuthRequest, res: Response) {
-	const searchParam = req.query;
-	const isEmptyObj = Object.keys(searchParam).length === 0;
-	try {
-		if (isEmptyObj) {
-			const users = await getAllUsers();
-			if (!users) return res.sendStatus(404);
-			return res.status(200).json({ results: users });
-		}
+  const searchParam = req.query;
+  const isEmptyObj = Object.keys(searchParam).length === 0;
+  try {
+    if (isEmptyObj) {
+      const users = await getAllUsers();
+      if (!users) return res.sendStatus(404);
+      return res.status(200).json({ results: users });
+    }
 
-		const role = searchParam.role as string;
+    const role = searchParam.role as string;
 
-		const lecturers = await getUserByRole(role);
+    const lecturers = await getUserByRole(role);
 
-		if (!lecturers.length) return res.sendStatus(404);
+    if (!lecturers.length) return res.sendStatus(404);
 
-		res.status(200).json({ results: lecturers });
-	} catch (error) {
-		console.log(error);
-		res.sendStatus(500);
-	}
+    res.status(200).json({ results: lecturers });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
 }
 
 async function handleAdminUpdateUser(req: Request, res: Response) {
-	const formObj = validateObject(req.body, adminUpdateShape);
+  const formObj = validateObject(req.body, adminUpdateShape);
 
-	if (formObj instanceof ZodError) {
-		const { message } = formObj.issues[0];
-		return res.status(STATUS.BAD_REQUEST.code).json(ErrorMsg(400, message));
-	}
+  if (formObj instanceof ZodError) {
+    const { message } = formObj.issues[0];
+    return res.status(STATUS.BAD_REQUEST.code).json(ErrorMsg(400, message));
+  }
 
-	try {
-		const { userId } = req.params;
-		const {
-			signature,
-			rankInClass,
-			classObtained,
-			numberOfGraduatedClass,
-			cwa,
-		} = formObj;
-		const findUser = await Users.findById(userId);
+  try {
+    const { userId } = req.params;
+    const {
+      signature,
+      rankInClass,
+      classObtained,
+      numberOfGraduatedClass,
+      cwa,
+    } = formObj;
+    const findUser = await Users.findById(userId);
 
-		if (!findUser) return res.status(404).json({ message: "User not found" });
+    if (!findUser) return res.status(404).json({ message: "User not found" });
 
-		await Users.updateOne(
-			{ _id: userId },
-			{
-				$set: {
-					signature,
-					rankInClass,
-					classObtained,
-					numberOfGraduatedClass,
-					cwa,
-				},
-			},
-			{ upsert: true, new: true },
-		);
+    await Users.updateOne(
+      { _id: userId },
+      {
+        $set: {
+          signature,
+          rankInClass,
+          classObtained,
+          numberOfGraduatedClass,
+          cwa,
+        },
+      },
+      { upsert: true, new: true }
+    );
 
-		res.status(200).json({ message: "User bluesheet updated successfully" });
-	} catch (error) {
-		console.log(error);
-	}
+    res.status(200).json({ message: "User bluesheet updated successfully" });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function handleDeleteUser(req: Request, res: Response) {
-	try {
-		const { userId } = req.params;
-		const foundUser = await Users.findById(userId);
+  try {
+    const { userId } = req.params;
+    const foundUser = await Users.findById(userId);
 
-		if (!foundUser) return res.status(404).json(ErrorMsg(404));
-		if (foundUser.role === "lecturer") {
-			await Users.deleteOne({ _id: userId });
-			return res.status(200).json({ message: "Lecturer deleted successfully" });
-		}
-		return res
-			.status(400)
-			.json(ErrorMsg(400, 'Only "lecturer" can be deleted'));
-	} catch (error) {
-		console.log(error);
-		res.status(500).json(ErrorMsg(500));
-	}
+    if (!foundUser) return res.status(404).json(ErrorMsg(404));
+    if (foundUser.role === "lecturer") {
+      await Users.deleteOne({ _id: userId });
+      return res.status(200).json({ message: "Lecturer deleted successfully" });
+    }
+    return res
+      .status(400)
+      .json(ErrorMsg(400, 'Only "lecturer" can be deleted'));
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(ErrorMsg(500));
+  }
 }
 
 export {
-	handleGetLoggedInUser,
-	handleGetUsers,
-	handleDeleteUser,
-	handleAdminUpdateUser,
+  handleGetLoggedInUser,
+  handleGetUsers,
+  handleDeleteUser,
+  handleAdminUpdateUser,
 };
